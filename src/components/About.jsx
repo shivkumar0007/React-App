4cb68d3242e270cf5de555556ba3f31130b6685a
Shiv Kumar Maurya <186371286+shiv2907@users.noreply.github.com>
2026-03-06 14:04:52 +0530

    navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,6 @@ const About = () => {
 {!hideNav && <Navbar/>}
     <div className={theme=="light"?"text-gray-800 flex flex-col items-center pt-20 pb-10" : "text-gray-200 flex flex-col items-center  pt-20"}>
       <h2 className="font-bold text-2xl underline mb-1">About Me</h2>
-
       <p>
         I am a passionate web developer who loves building
         modern responsive web applications using React,
